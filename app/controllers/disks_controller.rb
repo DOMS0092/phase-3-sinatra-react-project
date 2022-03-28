615e415f34826a604c21110c5711d0994e2cd823
kendra Hood <my.github.email@example.com>
2022-03-28 17:39:29 -0500

    build/ update routes

--- app/controllers/disks_controller.rb
+++ app/controllers/disks_controller.rb
@@ -2,36 +2,51 @@ class AlbumsController < ApplicationController
 
   # GET: /albums
   get "/albums" do
-    erb :"/albums/index.html"
+  Album.all.to_json
   end
 
   # GET: /albums/new
   get "/albums/new" do
-    erb :"/albums/new.html"
   end
 
   # POST: /albums
   post "/albums" do
-    redirect "/albums"
+    @album = Album.create(params)
+    if @album.id
+      serialized_album
+    else
+      @album.errors.full_messages.to_sentence
   end
 
   # GET: /albums/5
   get "/albums/:id" do
-    erb :"/albums/show.html"
+    find_album
+    if @plant
+      serialized_album
+    else
+      {errors: "Record not found with id #{params['id']}"}.to_json
   end
 
   # GET: /albums/5/edit
   get "/albums/:id/edit" do
-    erb :"/albums/edit.html"
   end
 
   # PATCH: /albums/5
   patch "/albums/:id" do
-    redirect "/albums/:id"
+    find_album
+  if @album && @album.update(params)
+    serialized_album
+  elsif !@album
+    {errors: "Record not found with id #{params['id']}"}.to_json
+  else
+    {errors: @album.errors.full_messages.to_sentence}.to_json
   end
+end 
 
   # DELETE: /albums/5/delete
-  delete "/albums/:id/delete" do
-    redirect "/albums"
+  delete "/albums/:id" do
+    find_album
+    if @album&.destroy
+      {messages: "Record successfully destroyed"}.to_json
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,6 @@ puts "🌱 Seeding spices..."
 a = Album.create(title: "4", total_time: 60, artist: "Beyonce", release_year: 2011)
 Track.create(title: "1+1", genre: "pop", explicit: false, track_length: 5, album: a)
 
-a = Album.create(title: "4", total_time: 60, artist: "Beyonce", release_year: 2011)
-Track.create(title: "Best Thing I Never Had", genre: "pop", explicit: false, track_length: 4, album: a)
 
 
 
